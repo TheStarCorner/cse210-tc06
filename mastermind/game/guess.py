ef7cb826e66ff6ba69f0548b4dd4902ec72c2285
Ammon Nelson <Ammon1nelson@gmail.com>
2021-05-26 16:08:10 -0600

    This is the guess class

---
+++ mastermind/game/guess.py
@@ -0,0 +1,24 @@
+class Guess:
+    ''' 
+        guess will get_guess and set_guess
+
+    sterotype: info holder
+
+
+
+    '''
+    
+    def __init__(self,guess):
+    
+        self.guess = guess
+
+
+    def get_guess(self):
+    
+        return self.guess
+
+    def set_guess(self,new_guess):
+        
+        self.guess = new_guess
+
+
